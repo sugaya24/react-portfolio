e181640b982096cdee903691c21ad39cb2339b08
YukiSugaya <sugaya324@gmail.com>
2020-11-10 03:55:43 -0800

    ADD: URLs in worksData

--- src/data/worksData.ts
+++ src/data/worksData.ts
@@ -6,6 +6,8 @@ export const worksData = [
     description:
       'This is Calendar app with React.js, redux, Firebase, styled-components.',
     devIcons: [],
+    projectUrl: 'https://react-calendar-c6a939.netlify.app/',
+    repositoryUrl: 'https://github.com/sugaya24/react-calendar',
   },
   {
     id: 2,
@@ -13,6 +15,8 @@ export const worksData = [
     projectImageUrl: 'img/weather_image.png',
     description: 'This is Calendar app with React.js.',
     devIcons: [],
+    projectUrl: 'https://uz46d.csb.app/',
+    repositoryUrl: 'https://codesandbox.io/s/react-weather-app-uz46d',
   },
   {
     id: 3,
@@ -20,6 +24,8 @@ export const worksData = [
     projectImageUrl: 'img/react_firebase_chat_image.png',
     description: 'This is Calendar app with React.js, Firebase, context API.',
     devIcons: [],
+    projectUrl: 'https://react-firebase-chat-01e283.netlify.app/',
+    repositoryUrl: 'https://github.com/sugaya24/react-firebase-chat',
   },
   {
     id: 4,
@@ -28,5 +34,7 @@ export const worksData = [
     description:
       'This is twitter clone with React.js, redux, Firebase,  Material-ui.',
     devIcons: [],
+    projectUrl: 'https://react-twitter-clone-d30dbd.netlify.app/',
+    repositoryUrl: 'https://github.com/sugaya24/react-twitter-clone',
   },
 ];
